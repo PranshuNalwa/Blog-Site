a3efb91b9830b94fc37efa6262d9459de8797c92
Pranshu Nalwa <pranshunalwa@Pranshus-MacBook-Air.local>
2026-03-04 02:19:21 +0530

    Add Postgres support for Vercel deployment

--- flasktut/config.py
+++ flasktut/config.py
@@ -4,7 +4,9 @@ load_dotenv()
 
 class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+        or os.environ.get('SQLALCHEMY_DATABASE_URI') \
+        or 'sqlite:///site.db'
     MAIL_SERVER='smtp.googlemail.com'
     MAIL_PORT= 587
     MAIL_USE_TLS= True
